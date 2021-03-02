fd0c111f7e7e3606501c30fe82da07586e0ead16
友廣 祐太朗 <tomohiro@fenrir.co.jp>
2021-03-02 23:03:30 +0900

    ユーザー削除機能の実装

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -44,6 +44,18 @@ module V1
       end
     end
 
+    def destroy
+      user = User.find_by_id(params[:id])
+
+      if !number?(params[:id])
+        response_bad_request
+      elsif user.nil?
+        response_not_found
+      elsif user.destroy
+        response_no_content
+      end
+    end
+
     private
       def user_params
         params.permit(:name)
