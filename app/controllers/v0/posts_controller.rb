50728bec0bee30bcba70594bc3056025e609a202
友廣 祐太朗 <tomohiro@fenrir.co.jp>
2021-03-08 15:02:31 +0900

    特定の投稿取得機能の実装

--- app/controllers/v0/posts_controller.rb
+++ app/controllers/v0/posts_controller.rb
@@ -1,14 +1,14 @@
 module V0
   class PostsController < ApplicationController
     def index
-      post = Post.where(user_id: params[:user_id])
+      posts = Post.where(user_id: params[:user_id])
 
       if !number?(params[:user_id])
         response_bad_request
-      elsif post.empty?
+      elsif posts.empty?
         response_not_found
       else
-        render json: post
+        render json: posts
       end
     end
 
@@ -20,6 +20,19 @@ module V0
         response_bad_request
       elsif post.save
         response_created
+      end
+    end
+
+    def show
+      user = User.find_by_id(params[:user_id])
+      post  = user&.post&.find_by_id(params[:id])
+
+      if !number?(params[:user_id]) || user.nil?
+        response_bad_request
+      elsif post.nil?
+        response_not_found
+      else
+        render json: post
       end
     end
 
--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,3 +1,3 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :title, :article
+  attributes :id, :title, :article
 end
